c51ed48ef460e4803e3678f53ca92bef94650394
yuu-ux <yuugaehara@gmail.com>
2025-05-17 02:15:29 +0900

    :heavy_plus_sign: Web を追加

--- app/controllers/web_controller.py
+++ app/controllers/web_controller.py
@@ -4,4 +4,8 @@ web_bp = Blueprint('web_bp', __name__)
 
 @web_bp.route('/', methods=['GET'])
 def index():
-    return render_template('web/index.html')
+    articles = [
+        {'id': 1, 'title': 'Using Flask with Docker', 'summary': 'A guide to containerizing your Flask app.'},
+        {'id': 2, 'title': 'Intro to Echo Framework', 'summary': 'Why Echo is a great choice for lightweight APIs.'},
+    ]
+    return render_template('web/index.html', articles=articles)
