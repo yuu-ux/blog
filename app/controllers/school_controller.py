03ca1f3f96acb5e50ef2f002285adcbea88f8bee
yuu-ux <yuugaehara@gmail.com>
2025-05-17 02:19:53 +0900

    :heavy_plus_sign: school 追加

--- app/controllers/school_controller.py
+++ app/controllers/school_controller.py
@@ -4,4 +4,8 @@ school_bp = Blueprint('school_bp', __name__)
 
 @school_bp.route('/', methods=['GET'])
 def index():
-    return render_template('school/index.html')
+    articles = [
+        {'id': 1, 'title': 'Using Flask with Docker', 'summary': 'A guide to containerizing your Flask app.'},
+        {'id': 2, 'title': 'Intro to Echo Framework', 'summary': 'Why Echo is a great choice for lightweight APIs.'},
+    ]
+    return render_template('school/index.html', articles=articles)
