126b7934945ea89532db8f1a01f4f5bb98ba78dc
yuu-ux <yuugaehara@gmail.com>
2025-05-17 21:28:47 +0900

    :heavy_plus_sign: wtforms 追加

---
+++ admin/forms/login.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    name = StringField('ユーザー名', validators=[DataRequired()])
+    password = PasswordField('パスワード', validators=[DataRequired()])
+    submit = SubmitField('ログイン')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
