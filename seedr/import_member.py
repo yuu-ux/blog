586dc110ab7cde44572837edd8ecb78433548b3c
yuu-ux <yuugaehara@gmail.com>
2025-05-17 23:37:31 +0900

    :wrench: ハッシュ化してDBに保存する

--- seedr/import_member.py
+++ seedr/import_member.py
@@ -1,6 +1,8 @@
 from setup_db import setup_db, AuditableColumns
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.mysql import INTEGER
+import bcrypt
+import hashlib
 
 app, db = setup_db()
 class Member(AuditableColumns, db.Model):
@@ -9,14 +11,19 @@ class Member(AuditableColumns, db.Model):
     name: Mapped[str] = mapped_column(db.String(100), nullable=False, default='')
     password: Mapped[str] = mapped_column(db.String(100), nullable=False, default='')
 
+def hash_password(password):
+    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    return bcrypt.hashpw(hashed_password.encode('utf-8'), bcrypt.gensalt())
+
 with app.app_context():
     db.create_all()
 
+    password = hash_password('hogehoge')
     if not db.session.get(Member, 1):
         member = Member(
             id=1, # type: ignore
-            name="yehara", # type: ignore
-            password="hogehoge", # type: ignore
+            name='yehara', # type: ignore
+            password=password, # type: ignore
         )
         db.session.add(member)
         db.session.commit()
