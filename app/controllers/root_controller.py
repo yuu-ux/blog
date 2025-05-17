e841aca5837d5f0e809e66eec86ee5815915888b
yuu-ux <yuugaehara@gmail.com>
2025-05-17 16:16:39 +0900

    :recycle: microcms 削除

--- app/controllers/root_controller.py
+++ app/controllers/root_controller.py
@@ -1,10 +1,7 @@
-import requests
 from flask import render_template, Blueprint
 
 root_bp = Blueprint('root_bp', __name__)
 
 @root_bp.route('/', methods=['GET'])
 def index():
-    response = requests.get('https://yehara.microcms.io/api/v1/blogs', headers={'X-MICROCMS-API-KEY': 'vkp24wfI3ZpvW1fZkS5OTxygmEAYYzJzG6y7'})
-    data = response.json().get('contents', [])
-    return render_template('index.html', data=data)
+    return render_template('index.html')
