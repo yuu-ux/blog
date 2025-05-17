ffb25295f1890e0ca39a2b327dbed967b879ab7c
yuu-ux <yuugaehara@gmail.com>
2025-05-17 23:37:04 +0900

    :wrench: ログイン機能実装

--- admin/controllers/login_controller.py
+++ admin/controllers/login_controller.py
@@ -1,7 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect, flash
+from forms.login import LoginForm
+from db.database import db
+from models.member import Member
+from sqlalchemy.exc import SQLAlchemyError
+import logging
 
 login_bp = Blueprint('login_bp', __name__)
 
-@login_bp.route('/', methods=['GET'])
+@login_bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        name = form.name.data # type: ignore
+        password = form.password.data # type: ignore
+        try:
+            member = db.session.query(Member).filter_by(name = name).first()
+            if member is None or not Member.check_password(password, member.password):
+                flash('ユーザー名またはパスワードが違います。', 'error')
+                return redirect(url_for('login_bp.index'))
+            else:
+                Member.login(member)
+                flash(f'{name}でログインしました')
+                return redirect(url_for('root_bp.index'))
+        except SQLAlchemyError:
+            logging.exception('login error')
+            form.name.data = '' # type: ignore
+            form.password.data = '' # type: ignore
+    return render_template('login.html', form=form)
+
+
--- admin/models/member.py
+++ admin/models/member.py
@@ -1,3 +1,6 @@
+import hashlib
+import bcrypt
+from flask import session
 from sqlalchemy.orm import Mapped, mapped_column
 from db.database import AuditableColumns, db
 from sqlalchemy.dialects.mysql import INTEGER
@@ -7,3 +10,12 @@ class Member(AuditableColumns, db.Model):
     id: Mapped[int] = mapped_column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(db.String(100), nullable=False, default='')
     password: Mapped[str] = mapped_column(db.String(100), nullable=False, default='')
+
+    @staticmethod
+    def login(member):
+        session['member_id'] = member.id
+
+    @staticmethod
+    def check_password(input_password, target_hashed_password):
+        input_sha256_hashed_password = hashlib.sha256(input_password.encode('utf-8')).hexdigest()
+        return bcrypt.checkpw(input_sha256_hashed_password.encode('utf-8'), target_hashed_password.encode('utf-8'))
