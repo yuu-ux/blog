73cca8db0c13e8d9591a437faad9995ecb0049eb
yuu-ux <yuugaehara@gmail.com>
2025-05-18 17:04:38 +0900

    :wrench: データベースの中の記事を表示できるようにする

--- app/controllers/school_controller.py
+++ app/controllers/school_controller.py
@@ -1,11 +1,10 @@
 from flask import render_template, Blueprint
+from common.db.database import db
+from common.models.post import Post
 
 school_bp = Blueprint('school_bp', __name__)
 
 @school_bp.route('/', methods=['GET'])
 def index():
-    articles = [
-        {'id': 1, 'title': 'Using Flask with Docker', 'summary': 'A guide to containerizing your Flask app.'},
-        {'id': 2, 'title': 'Intro to Echo Framework', 'summary': 'Why Echo is a great choice for lightweight APIs.'},
-    ]
-    return render_template('school/index.html', articles=articles)
+    posts = db.session.query(Post).filter(Post.category_id == 2).all()
+    return render_template('school/index.html', posts=posts)
--- app/controllers/web_controller.py
+++ app/controllers/web_controller.py
@@ -1,12 +1,10 @@
 from flask import render_template, Blueprint
-from db.database import db
+from common.db.database import db
+from common.models.post import Post
 
 web_bp = Blueprint('web_bp', __name__)
 
 @web_bp.route('/', methods=['GET'])
 def index():
-    articles = [
-        {'id': 1, 'title': 'Using Flask with Docker', 'summary': 'A guide to containerizing your Flask app.'},
-        {'id': 2, 'title': 'Intro to Echo Framework', 'summary': 'Why Echo is a great choice for lightweight APIs.'},
-    ]
-    return render_template('web/index.html', articles=articles)
+    posts = db.session.query(Post).filter(Post.category_id == 1).all()
+    return render_template('web/index.html', posts=posts)
