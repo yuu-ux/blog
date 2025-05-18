01c7dac25a7c2bba9f12d3c3703ba932f01afea5
yuu-ux <yuugaehara@gmail.com>
2025-05-18 14:13:43 +0900

    :wrench: 編集機能追加

--- admin/controllers/post_controller.py
+++ admin/controllers/post_controller.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, redirect, url_for, render_template, session, g, flash
+from flask import Blueprint, redirect, url_for, render_template, request, g, flash
 from db.database import db
 from models.post import Post
 from forms.post import PostForm
+from sqlalchemy.exc import SQLAlchemyError
+import logging
 
 
 post_bp = Blueprint('post_bp', __name__)
@@ -11,31 +13,41 @@ def index(post_id):
     if not g.member:
         return redirect(url_for('login_bp.index'))
 
-    form = PostForm()
-    post = db.session.get(Post, post_id)
+    post = db.session.query(Post).filter(Post.id == post_id, Post.is_deleted != True).first()
     if post is None:
         flash('記事を取得できませんでした', 'error')
         return redirect('root_bp.index')
-    return render_template('posts/index.html', member=g.member, post=post, form=form)
+    return render_template('posts/index.html', member=g.member, post=post)
 
-# @post_bp.route('/<int:post_id>/edit', methods=['GET', 'POST'])
-# def edit(post_id):
-#     if not g.member:
-#         return redirect(url_for('login_bp.index'))
-#
-#     form = PostForm()
-#     post = db.session.get(Post, post_id)
-#     if post is None:
-#         return
-#     form.body.data = post.data
-#     if not form.validate_on_submit():
-#         return redirect('post_bp.detail')
-#
-#     post = db.session.get(Post, post_id)
-#     post.body = input
-#     db.session.commit()
-#     flash('記事を更新しました', 'info')
-#     return redirect(url_for('post_bp.detail'))
+@post_bp.route('/<int:post_id>/edit', methods=['GET', 'POST']) #type: ignore
+def edit(post_id):
+    if not g.member:
+        return redirect(url_for('login_bp.index'))
+
+    post = db.session.query(Post).filter(Post.id == post_id, Post.is_deleted != True).first()
+    if post is None:
+        flash('記事を取得できませんでした', 'error')
+        logging.error('error get post')
+        return redirect('root_bp.index')
+
+    form = PostForm()
+    if request.method == 'GET':
+        form.title.data = post.title # type: ignore
+        form.body.data = post.body # type: ignore
+
+    if form.validate_on_submit():
+        try:
+            post.title = form.title.data # type: ignore
+            post.body = form.body.data # type: ignore
+            db.session.commit()
+        except SQLAlchemyError:
+            logging.error('error post edit')
+            flash('記事の更新に失敗しました', 'error')
+            return redirect(url_for('post_bp.index', post_id=post.id))
+        flash('記事を更新しました', 'info')
+        return redirect(url_for('post_bp.index', post_id=post.id))
+
+    return render_template('posts/edit.html', member=g.member, post=post, form=form)
 #
 #
 # @post_bp.route('/delete/<int:post_id>', methods=['POST'])
--- admin/forms/post.py
+++ admin/forms/post.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
     title = StringField('タイトル', validators=[DataRequired()])
-    body = TextAreaField()
-    submit = SubmitField()
+    body = TextAreaField('本文')
+    category = SelectField('カテゴリ', choices=[
+        ('web', 'web'),
+        ('school', '42Tokyo'),
+    ])
+    submit = SubmitField('更新')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
