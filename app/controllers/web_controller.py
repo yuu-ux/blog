62a4e537249216e3865fa037665c7ccbb8a4bafd
yuu-ux <yuugaehara@gmail.com>
2025-05-19 20:40:50 +0900

    :heavy_plus_sign: 詳細ページ追加

--- app/controllers/web_controller.py
+++ app/controllers/web_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, url_for, redirect, flash
 from common.db.database import db
 from common.models.post import Post
 from config import CATEGORY_WEB
@@ -8,5 +8,22 @@ web_bp = Blueprint('web_bp', __name__)
 
 @web_bp.route('/', methods=['GET'])
 def index():
-    posts = db.session.query(Post).filter(Post.category_id == CATEGORY_WEB, Post.is_deleted != True).all()
+    posts = (
+        db.session.query(Post)
+        .filter(Post.category_id == CATEGORY_WEB, Post.is_deleted != True)
+        .all()
+    )
     return render_template('web/index.html', posts=posts)
+
+
+@web_bp.route('/<int:post_id>', methods=['GET'])
+def detail(post_id):
+    post = (
+        db.session.query(Post)
+        .filter(Post.id == post_id, Post.is_deleted != True)
+        .first()
+    )
+    if post is None:
+        flash('記事が見つかりませんでした', 'error')
+        return redirect(url_for('web_bp.index'))
+    return render_template('web/detail.html', post=post)
