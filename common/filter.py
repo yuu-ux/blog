3ad25a3ee4225eb0a4803adf8ea6fa6e0b787e88
yuu-ux <yuugaehara@gmail.com>
2025-05-23 15:37:39 +0900

    :wrench: リンクを自動で認識する機能を追加

--- common/filter.py
+++ common/filter.py
@@ -1,15 +1,34 @@
 from datetime import datetime
 from markdown import markdown
 from markupsafe import Markup
+import bleach
+from urllib.parse import urlparse
+from bleach.linkifier import Linker
+
 
 def setup_filter(app):
     app.jinja_env.filters['format_date'] = format_date
     app.jinja_env.filters['markdown'] = markdown_filter
+
 
 def format_date(value):
     if isinstance(value, datetime):
         return value.strftime('%Y-%m-%d')
     return value
 
+
+def set_target(attrs, new=False):
+    p = urlparse(attrs[(None, 'href')])
+    if p.netloc not in ['localhost']:
+        attrs[(None, 'target')] = '_blank'
+        attrs[(None, 'class')] = 'external'
+    else:
+        attrs.pop((None, 'target'), None)
+    return attrs
+
+
 def markdown_filter(text):
-    return Markup(markdown(text))
+    html = markdown(text, extensions=['extra', 'fenced_code'])
+    linker = Linker(callbacks=[set_target], skip_tags=['pre', 'code'])
+    html = linker.linkify(html)
+    return Markup(html)
