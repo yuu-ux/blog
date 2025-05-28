2ccc937325702a3bf7868de1f6026e9979f9972c
yuu-ux <yuugaehara@gmail.com>
2025-05-28 12:53:20 +0900

    :wrench: config の変数を参照してDBに接続できるようにする

--- seeder/imports/setup_db.py
+++ seeder/imports/setup_db.py
@@ -4,7 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from datetime import datetime
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent.parent))
 
 from common.config import DB_URI
 
@@ -12,16 +13,25 @@ from common.config import DB_URI
 class Base(DeclarativeBase):
     pass
 
+
 def setup_db():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-    app.secret_key = 'secret'
+    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+    app.secret_key = "secret"
     db = SQLAlchemy(model_class=Base)
     db.init_app(app)
     return app, db
 
+
 app, db = setup_db()
 
+
 class AuditableColumns:
-    created_on: Mapped[datetime] = mapped_column(db.DateTime(6), nullable=False, default=datetime.now)
-    modified_on: Mapped[datetime] = mapped_column(db.TIMESTAMP, nullable=False, server_default=db.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+    created_on: Mapped[datetime] = mapped_column(
+        db.DateTime(6), nullable=False, default=datetime.now
+    )
+    modified_on: Mapped[datetime] = mapped_column(
+        db.TIMESTAMP,
+        nullable=False,
+        server_default=db.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
+    )
